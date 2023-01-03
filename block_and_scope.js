4300ce7e579f127c7d1f88ac5775ecf8bc93958d
abhi <programwithabhi@gmail.com>
2023-01-03 19:44:12 +0530

    block scope and the function scope understand

---
+++ block_and_scope.js
@@ -0,0 +1,9 @@
+// A block is the compound statement that the javascript wether you can use it to combine multiple statements.
+
+var abhi = 'abhi'
+{
+    var abhi = 'abhi2'
+}
+console.log(abhi)
+
+// a();
