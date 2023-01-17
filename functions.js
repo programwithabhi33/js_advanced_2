77d3f1f6e4da900af53bf181204e748c405bb095
abhi <programwithabhi@gmail.com>
2023-01-17 21:56:38 +0530

    leaned function in the javascript also first class functions

---
+++ functions.js
@@ -0,0 +1,37 @@
+// a();
+// b();
+// function statement or function declaration
+// function a(){
+//     console.log("a called")
+// }
+
+
+// // function expression:- it means you can assign a function into any variable
+// let b = function (){
+//     console.log("b called")
+// }
+
+
+// anonymous functions:- A functions that does not have name is called anonymous function's
+// Note: anonymous functions only used when it's need a function 
+// You can't use anonymous functions in function statement and in function declaration this willl give you error 
+// function(){
+
+// }
+
+// Named functions expression:- A named function expression is you can assign a named function into any variable 
+// Note:- You can't access the named function to call the function this will give you error(e.g.acb()) you can only use the variable that the function has been assigned 
+// let abhi = function abc (){
+//     console.log('this is abc function assigned to the abhi variable')
+// }
+
+// function abhi(){
+//     return function(){
+//         console.log("abhishek")
+//     }
+// }
+
+
+// First class functions means that you can pass a function as an argument to another function and you can return a brand new function (anonymous function) from that called function this ability in javascript or any other programming language is called first class functions 
+
+// abhi()();
