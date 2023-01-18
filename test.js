8e2c77117f48d0930a53a9a7559f9b85db663889
abhi <programwithabhi@gmail.com>
2023-01-18 22:33:53 +0530

    added test.js files

---
+++ test.js
@@ -0,0 +1,15 @@
+let obj = {
+    name:"abhishek",
+    age:80,
+}
+
+let arr = ["abhishek",'rushikesh','ankit']
+
+// console.log(obj?.abhi)
+// console.log(arr.name)
+
+async function abhishek(){
+    let data = await fetch("./test2.js")
+    console.log(data)
+}
+abhishek();
