c88d3b98dcebe2ccc9519864b04dc1ef302cc2c9
abhi <programwithabhi@gmail.com>
2023-01-18 22:38:57 +0530

    callback functions understand

---
+++ callback.js
@@ -0,0 +1,7 @@
+let element = document.getElementById("main")
+
+element.addEventListener("click",function(){
+    console.log("Clicked on the main id element")
+})
+
+// callback function are those functions we passed another function or statement a function and they execute the callback function when the event occurs or the time expires
