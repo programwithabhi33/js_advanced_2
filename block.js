59c51555a90ad5f1bfd3f23f3e91d12178cfe400
abhi <programwithabhi@gmail.com>
2023-01-18 22:49:36 +0530

    block scope and statement in javscript

---
+++ block.js
@@ -0,0 +1,10 @@
+{
+    // Also knows as compound statement 
+    // block contains multiples statements together and combined into group that is also know as a block 
+    // Block contain multiple statements and combined to one statement 
+    // example 
+    let abhi = "abhishek"; //statemtnt
+    let b = "b" //another statement 
+
+    // it combined these two or more statements are combined to one statement 
+}
