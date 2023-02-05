7464127e1e03b146c197bfad548b7722cd663cc6
abhi <programwithabhi@gmail.com>
2023-02-05 18:33:19 +0530

    understand the concept of higher order functions and also how you can chain this in any datatype Prototype

--- higher_order_functions.js
+++ higher_order_functions.js
@@ -1,12 +1,54 @@
 // higher order functions are those functions in javascript that takes another function as a parameter or returned any function to return value
 
-function calc(fun){
-    return fun;
+// function calc(fun){
+//     return fun;
+// }
+
+// function fun(){
+//     console.log("Fun function called")
+// }
+
+// let another_fun = calc(fun)
+// another_fun();
+
+
+// You can use the higer order function to write less code and extract logic and abstract your code
+// radius = [1,2,3,4,5]
+// function area(radius){
+//     return Math.PI * radius *radius;
+// }
+
+// function calculate(radius,logic){
+//     let output = [];
+//     radius.forEach(element => {
+//         output.push(logic(element))
+//     });
+//     return output;
+// }
+// console.log(calculate(radius,area))
+
+
+
+
+function area(radius){
+    return Math.PI * radius *radius;
+}
+let radius = [1, 2, 3, 4]
+function calculate(logic) {
+    console.log(this)
+    // let output = [];
+    // radius.forEach(element => {
+    //     output.push(logic(element))
+    // });
+    // return output;
 }
 
-function fun(){
-    console.log("Fun function called")
-}
+// You can assign any function to any datatype prototype so you can use this assigned function that datatype's 
+Array.prototype.calculate = calculate;
+console.log(radius)
 
-let another_fun = calc(fun)
-another_fun();
+radius.calculate(area)
+
+
+
+
